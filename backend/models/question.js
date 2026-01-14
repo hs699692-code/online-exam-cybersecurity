5606013da0654393b57b9d176cea178973cbc77b
Hitesh <hs699692@gmail.com>
2026-01-14 09:54:05 +0530

    Create question.js

---
+++ backend/models/question.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const QuestionSchema = new mongoose.Schema({
+  question: { type: String, required: true },
+  options: { type: [String], required: true },
+  answer: { type: String, required: true },
+  chapter: { type: String }
+});
+
+module.exports = mongoose.model("Question", QuestionSchema);
