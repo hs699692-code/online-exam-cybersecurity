66ecd353eeb6c654b3317fe218625fee003a7366
Hitesh <hs699692@gmail.com>
2026-01-14 13:59:15 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,23 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://onlineexam_db_user:axMvbMqSjAx6HrSF@onlinetestcluster0.rzmm3um.mongodb.net/?appName=onlinetestCluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-};
-
-module.exports = connectDB;
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
