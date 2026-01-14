e68186b209eabad355a458c8883840196869f12f
Hitesh <hs699692@gmail.com>
2026-01-14 09:53:38 +0530

    Create student.js

---
+++ backend/models/student.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const StudentSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  roll: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+module.exports = mongoose.model("Student", StudentSchema);
